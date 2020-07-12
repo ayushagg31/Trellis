f142ad2da581ffb7eec13c6f0126b94aec29cc19
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-07-12 19:46:46 +0530

    middleware setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,30 @@
-/* eslint-disable no-unused-vars */
 const express = require('express')
+/* eslint-disable-next-line no-unused-vars */
 const cors = require('cors')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const { notFoundHandler, errorHandler } = require('./middleware')
+
+require('dotenv').config()
 
 const app = express()
-const PORT = 8080
+const PORT = 8080 || process.env.PORT
+
+app.use(morgan('tiny'))
+app.use(helmet())
+// app.use(cors({
+//     origin: process.env.CORS_URL,
+// }))
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'success'
-    }).status(201)
+        message: 'success',
+        staus: res.statusCode
+    })
 })
+
+app.use(notFoundHandler)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
---
+++ server/src/middleware.js
@@ -0,0 +1,21 @@
+const notFoundHandler = ('*', (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
+
+/* eslint-disable-next-line no-unused-vars */
+const errorHandler = (error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: error.message,
+        stack: error.stack,
+        status: statusCode
+    })
+}
+
+module.exports = {
+    notFoundHandler,
+    errorHandler,
+}
