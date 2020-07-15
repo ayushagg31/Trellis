778afb014ea209f38ccd521ca36ad3ff14d362c6
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-07-15 23:14:27 +0530

    tests updated

--- server/test/board.test.js
+++ server/test/board.test.js
@@ -3,20 +3,29 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Board = require('../src/models/board')
-const {boardTwo, setupBoard} = require('./fixtures/db')
+const { boardTwo } = require('./fixtures/db')
 
-
-beforeEach(setupBoard)
-
-
-test('Should create a new board', async () => {
-    await request(app).post('/api/boards').send(boardTwo).expect(200)
+describe('POST@/api/boards', () => {
+    it('Should create a new board', async () => {
+        await Board.deleteMany()
+        await request(app).post('/api/boards').send(boardTwo).expect(200)
+    })
+    it('Should not create board without name', async () => {
+        await request(app).post('/api/boards').send({}).expect(422)
+    })
 })
 
-test('Should show all boards', async () => {
-    const resp = await request(app).get('/api/boards').send().expect(200)
-    const boardEntries = await Board.find({})
-    expect(JSON.stringify(resp.body)).toEqual(JSON.stringify(boardEntries))
-    
+describe('Get@/api/boards', () => {
+    it('should show add the boards from db', async () => {
+        const resp = await request(app).get('/api/boards').send().expect(200)
+        const boardEntries = await Board.find({})
+        expect(JSON.stringify(resp.body)).toEqual(JSON.stringify(boardEntries))
+    })
+    it('Should show empty board-db', async () => {
+        await Board.deleteMany()
+        const resp = await request(app).get('/api/boards').send().expect(200)
+        const boardEntries = await Board.find({})
+        expect(JSON.stringify(resp.body)).toEqual(JSON.stringify(boardEntries))
+    })
 })
 
--- server/test/list.test.js
+++ server/test/list.test.js
@@ -3,20 +3,46 @@
 const request = require('supertest')
 const app = require('../src/app')
 const List = require('../src/models/list')
-const { listTwo, setupList } = require('./fixtures/db')
+const Board = require('../src/models/board')
+const { boardOneId, boardOne, listOne, listTwo } = require('./fixtures/db')
 
-beforeEach(
-    setupList
-)
 
-test('Should create a new list', async () => {
-    await request(app).post('/api/lists').send(listTwo).expect(200)
+describe('POST@/api/lists', () => {
+    it('Should create a new list', async () => {
+        await Board.deleteMany()
+        await new Board(boardOne).save()
+        await List.deleteMany()
+        await request(app).post('/api/lists').send(listOne).expect(200)
+    })
 
+    it('Should not create list without any data', async () => {
+        await request(app).post('/api/lists').send({}).expect(422)
+    })
+
+    it('Should not create list without name', async () => {
+        await request(app).post('/api/lists').send({ boardId: boardOneId }).expect(422)
+    })
+    it('Should not create list without boardId', async () => {
+        await request(app).post('/api/lists').send({ name: 'listTestOne' }).expect(422)
+    })
+    it('Should not create list with invalid boardId', async () => {
+        await request(app).post('/api/lists').send({ name: 'listTestOne', boardId: 'randomBoardId' }).expect(422)
+
+    })
 })
 
-test('Should show all lists', async () => {
-    const resp = await request(app).get('/api/lists').send().expect(200)
-    const listEntries = await List.find({})
-    expect(JSON.stringify(resp.body)).toEqual(JSON.stringify(listEntries))
+describe('Get@/api/lists', () => {
+    it('Should show all lists', async () => {
+        const resp = await request(app).get('/api/lists').send().expect(200)
+        const listEntries = await List.find({})
+        expect(JSON.stringify(resp.body)).toEqual(JSON.stringify(listEntries))
+    })
+    it('Should show empty list-db', async () => {
+        await List.deleteMany()
+        const resp = await request(app).get('/api/lists').send().expect(200)
+        const listEntries = await List.find({})
+        expect(JSON.stringify(resp.body)).toEqual(JSON.stringify(listEntries))
+    })
 })
+
 
