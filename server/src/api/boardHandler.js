c79247b905071d48fe89e218233c0f3ee61a25d8
harsh <harsh7oct@gmail.com>
2020-07-21 21:31:27 +0530

    updated api

--- server/src/api/boardHandler.js
+++ server/src/api/boardHandler.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const Board = require('../models/board')
+const List = require('../models/list')
 
 const router = Router()
 
-
+// fetch all the board entries
 router.get('/', async (req, res, next) => {
     try {
         const boardsList = await Board.find()
@@ -13,6 +14,7 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+// create new board entry
 router.post('/', async (req, res, next) => {
     try {
         const board = new Board(req.body)
@@ -21,11 +23,35 @@ router.post('/', async (req, res, next) => {
     } catch (error) {
         if (error.name === 'ValidationError')
             res.status(422)
-        else
-            res.status(400)
         next(error)
     }
 })
 
-module.exports = router
+// get board based on id
+router.get('/:id', async (req, res, next) => {
+    const _id = req.params.id
+    try {
+        const board = await Board.findById(_id)
+        if (!board)
+            return res.status(404).send()
+        res.send(board)
+    } catch (error) {
+        next(error)
+    }
+})
 
+router.get('/:id/lists', async (req, res, next) => {
+    const _id = req.params.id
+    try {
+        const board = await Board.findById(_id)
+        if (!board)
+            return res.status(404).send()
+        const lists = await List.find({ boardId: _id })
+        res.send(lists)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+module.exports = router
