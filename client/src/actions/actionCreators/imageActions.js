7fddb4063fadb9ad34b10ddb83e74990399ee96c
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-08-17 10:20:49 +0530

    Update imageActions.js

--- client/src/actions/actionCreators/imageActions.js
+++ client/src/actions/actionCreators/imageActions.js
@@ -4,7 +4,7 @@ import * as ACTIONS from '../actions'
 
 const page = Math.floor(Math.random() * 100) + 1
 const query = 'landscape'
-const BASE_URL = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=SZJuHTFL-JO31E435i4CZOuyZLlbnX1SZyiPkF3kE2Q`
+const BASE_URL = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&`
 
 export const fetchImages = () => {
     return (dispatch) => {
