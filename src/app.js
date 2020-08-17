ab8d10ab9c7fd473e1cb38f7fd904811d44285d2
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-08-18 00:43:24 +0530

    fixed

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../client/build'))
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
     })
 }
