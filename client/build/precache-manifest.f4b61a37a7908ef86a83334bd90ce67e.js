e14f7286bbc8de69574d7558d3d7c238fba99733
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-08-18 01:16:30 +0530

    fixed

---
+++ client/build/precache-manifest.f4b61a37a7908ef86a83334bd90ce67e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861098d4814b2df6f5fe72be71d70fa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c113c493f28d9f617b3b",
+    "url": "/static/css/main.5a06fd0a.chunk.css"
+  },
+  {
+    "revision": "804d5904eacae90b1881",
+    "url": "/static/js/2.f46266d2.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.f46266d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c113c493f28d9f617b3b",
+    "url": "/static/js/main.fcf46f79.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
