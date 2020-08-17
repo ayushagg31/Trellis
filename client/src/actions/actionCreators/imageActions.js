752ea9cea5dfd5c3b1e3c388d6e2f2f80e1617f9
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-08-17 10:36:04 +0530

    clientApi added

--- client/src/actions/actionCreators/imageActions.js
+++ client/src/actions/actionCreators/imageActions.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 import * as ACTIONS from '../actions'
 
-
 const page = Math.floor(Math.random() * 100) + 1
 const query = 'landscape'
-const BASE_URL = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=SZJuHTFL-JO31E435i4CZOuyZLlbnX1SZyiPkF3kE2Q`
+const BASE_URL = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${process.env.REACT_APP_CLIENT_KEY}`
 
 export const fetchImages = () => {
     return (dispatch) => {
