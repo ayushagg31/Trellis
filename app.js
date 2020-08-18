569bc46c6202271b9ce4e624e04e070fabb08345
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-08-18 18:50:18 +0530

    typo error

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build/index.html'))
     })
-},
+}
 app.use(notFoundHandler)
 
 module.exports = app
