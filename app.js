76545275a372683b5a128bf9f7f33c2b2fc92f28
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-08-18 17:13:39 +0530

    fixed

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use('/api/activities/', activityHandler)
 app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
-app.use(express.static('client/build'))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'))
-})
-
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build/index.html'))
+    })
+}
 // app.use('/api', apiHandler)
 
 app.use(notFoundHandler)
