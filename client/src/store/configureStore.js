9ede4ebb66073b26530ccac04c48ec9c099a8a98
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-08-21 16:19:35 +0530

    animations issue resolved

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -31,6 +31,6 @@ export default createStore(
     rootReducer,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 )
