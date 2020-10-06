1653cae088500503415a9de6459102e37a02c3d3
El Mehdi Boumhicha <96.elmehdi@gmail.com>
2020-10-06 23:18:21 +0200

    Improve the syntax in activityActions file

--- client/src/actions/actionCreators/activityActions.js
+++ client/src/actions/actionCreators/activityActions.js
@@ -4,29 +4,25 @@ import * as ACTIONS from '../actions'
 const BASE_URL = '/api/activities/'
 
 
-export const createNewActivity = (params, token) => {
-    return (dispatch) => {
-        axios.post(BASE_URL, params, {
-            headers: { 'x-auth-token': token }
-        }).then(res => {
-            dispatch({ type: ACTIONS.ADD_ACTIVITY, payload: { activity: res.data } })
-        }).catch(e => {
-            if (e.message === "Network Error")
-                dispatch({ type: ACTIONS.ERROR_ACTIVITY, payload: { error: e } })
-            else if (e.response.status === 422)
-                dispatch({ type: ACTIONS.VALIDATION_ERROR_ACTIVITY })
-        })
-    }
+export const createNewActivity = (params, token) => (dispatch) => {
+    axios.post(BASE_URL, params, {
+        headers: { 'x-auth-token': token }
+    }).then(res => {
+        dispatch({ type: ACTIONS.ADD_ACTIVITY, payload: { activity: res.data } })
+    }).catch(e => {
+        if (e.message === "Network Error")
+            dispatch({ type: ACTIONS.ERROR_ACTIVITY, payload: { error: e } })
+        else if (e.response.status === 422)
+            dispatch({ type: ACTIONS.VALIDATION_ERROR_ACTIVITY })
+    })
 }
 
 
-export const deleteActivityById = (id) => {
-    return (dispatch) => {
-        axios.delete(BASE_URL + id)
-            .then(res => {
-                dispatch({ type: ACTIONS.DELETE_ACTIVITY, payload: { activity: res.data } })
-            }).catch(e => {
-                dispatch({ type: ACTIONS.ERROR_ACTIVITY, payload: { error: e } })
-            })
-    }
+export const deleteActivityById = (id) => (dispatch) => {
+    axios.delete(BASE_URL + id)
+        .then(res => {
+            dispatch({ type: ACTIONS.DELETE_ACTIVITY, payload: { activity: res.data } })
+        }).catch(e => {
+            dispatch({ type: ACTIONS.ERROR_ACTIVITY, payload: { error: e } })
+        })
 }
